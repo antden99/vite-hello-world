5f5eace274c8ef2bec9f2b16b0d3b61cb48c6ebf
antden99 <antoniodenigris@icloud.com>
2024-03-20 15:57:04 +0100

    first commit add structure and Hello World in template

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script >
+  export default {
+    data(){
+      return {
+        text: "Hello World"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{text}}</h1>
+   
+</template>
+
+<style >
+
+</style>
