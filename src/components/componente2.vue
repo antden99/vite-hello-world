5e0e9492df74e948c8daf284cebe00af0f73d579
antden99 <antoniodenigris@icloud.com>
2024-03-20 17:39:35 +0100

    add component 2

---
+++ src/components/componente2.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Ciao Anto",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<style>
+
+</style>
